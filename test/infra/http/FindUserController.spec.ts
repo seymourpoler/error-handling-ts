fe468332a8ef799effeb197e29e361c9063e9a95
Luis Rodríguez <luis.rodriguez@leanmind.es>
2024-05-30 17:10:39 +0200

    red: return user, if user is found

--- test/infra/http/FindUserController.spec.ts
+++ test/infra/http/FindUserController.spec.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { createRequest, createResponse, MockRequest, MockResponse } from 'node-mocks-http';
 import { FindUserUseCase } from "../../../src/application/User/FindUserUseCase";
 import { FindUserController } from '../../../src/infra/http/FindUserController';
+import { User } from '../../../src/domain/User';
 
 describe('FindUserUseCase Should', () => {
     let findUser: TypeMoq.IMock<FindUserUseCase>;
@@ -18,9 +19,20 @@ describe('FindUserUseCase Should', () => {
     const anyRequest: MockRequest<Request> = createRequest({body:{username}});
     const anyResponse: MockResponse<Response> = createResponse();
 
-    const response = await controller.execute({ username });
+    const response = await controller.execute(anyRequest, anyResponse);
 
-    expect(response.statusCode).toBe(400)
+    expect(response.statusCode).toBe(404)
   });
 
+  it('return user, if user is found', async () => {
+    const username = 'any-user-name';
+    const anyRequest: MockRequest<Request> = createRequest({body:{username}});
+    const anyResponse: MockResponse<Response<User>> = createResponse();
+    const anyUser = new User('any-user-name', 'any-password', 'admin');
+    findUser.setup(x => x.execute(TypeMoq.It.isAny())).returns(() => anyUser);
+
+    const response = await controller.execute(anyRequest, anyResponse);
+
+    expect(response.statusCode).toBe(200);
+  });
 });
