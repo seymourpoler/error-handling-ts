86fd5ec43b77ee1fe4000d27a8409ef09b5eb121
Luis Rodríguez <luis.rodriguez@leanmind.es>
2024-07-19 18:05:53 +0200

    add user repository orm

--- src/infra/database/PostgresUserRepository.ts
+++ src/infra/database/PostgresUserRepository.ts
@@ -5,8 +5,9 @@ import { CreateUserResult } from "../../application/User/CreateUserResult";
 export interface UserRepository {
     exists(user: User): boolean;
     countOfAdmins(): number;
-    save(user: User): CreateUserResult;
+    save(user: User): Promise<CreateUserResult> ;
     find(email: string) : Promise<User | null>;
+    delete(email: string): Promise<void>
 }
 
 export class PostgresUserRepository implements UserRepository {
@@ -22,25 +23,39 @@ export class PostgresUserRepository implements UserRepository {
     }
 
     public countOfAdmins(): number {
-        return this.users.filter(u => u.isAdmin()).length
+        const administrators = this.users.filter(u => u.isAdmin());
+        return administrators.length
     }
 
-    public save(user: User): CreateUserResult {
-        throw new Error("Method not implemented.");
+    public async save(user: User): Promise<CreateUserResult> {
+        const connection = this.connectionFactory.create();
+        const users = await connection`
+            INSERT 
+            INTO public.users (email, password, role)
+            VALUES (${user.email}, ${user.password}, ${user.role})`;
+        
+        return CreateUserResult.success();
     }
 
     public async find(email: string) : Promise<User | null> {
         const connection = this.connectionFactory.create();
-
         const users = await connection<User[]>`
-              SELECT *
-             FROM public.users
-             WHERE users.email = ${email + '%'}`;
+            SELECT *
+            FROM public.users
+            WHERE users.email = % ${email} %`;
 
         if(users.length === 0){
             return null;
         }
 
         return users[0]
+    }
+
+    public async delete(email: string): Promise<void> {
+        const connection = this.connectionFactory.create();
+        const users = await connection`
+            DELETE
+            FROM public.users
+            WHERE users.email = % ${email} %`;
     }
 }
--- test/infra/database/UserRepository.spec.ts
+++ test/infra/database/UserRepository.spec.ts
@@ -1,4 +1,4 @@
-import { describe, beforeEach , it, expect } from "vitest";
+import { describe, beforeEach , it, expect, afterEach } from "vitest";
 import { Configuration } from "../../../src/infra/database/Configuration";
 import { ConnectionFactory } from "../../../src/infra/database/ConnectionFactory";
 import { UserRepository, PostgresUserRepository } from "../../../src/infra/database/PostgresUserRepository";
@@ -6,22 +6,25 @@ import { User } from "../../../src/domain/User";
 
 describe('User repository', () =>{
     let userRepository: UserRepository;
+    const anEmail = "e@ma.il";
 
     beforeEach(() =>{
         const connectionFactory = new ConnectionFactory(new Configuration());
         userRepository = new PostgresUserRepository(connectionFactory);
+        userRepository.save(new User(anEmail, "password", "admin"));
     });
 
-
     describe.skip('Find user should', () => {
-        it('return found user', async () =>{
-            const anEmail = "e@ma.il";
-            
+        it('return found user', async () =>{          
             const user = await userRepository.find(anEmail);
 
             expect(user).not.toBeNull();
             expect(user).toBeInstanceOf(User);
             expect(user?.email).toBe(anEmail);
         });
+    });
+
+    afterEach(() =>{
+        userRepository.delete(anEmail);
     });
 });
